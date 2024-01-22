ba5ae40372d41d6ab21122116325da322373194f
djpont <alexandervotyakov@gmail.com>
2024-01-23 03:46:35 +0400

    feat: add try-catch

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-const content = fs.createReadStream(path.join(__dirname, 'text.txt'));
-content.setEncoding('UTF8');
-content.forEach((chunk) => {
-  console.log(chunk);
-});
+try {
+  const content = fs.createReadStream(path.join(__dirname, 'text.txt'));
+  content.setEncoding('UTF8');
+  content.forEach((chunk) => {
+    console.log(chunk);
+  });
+} catch (err) {
+  console.error(err);
+}
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,31 +3,35 @@ const path = require('path');
 const { stdin } = require('node:process');
 const { Readable } = require('stream');
 
-const writing = fs.createWriteStream(path.join(__dirname, 'text.txt'), {
-  encoding: 'utf8',
-});
-const reading = Readable.from(stdin);
+try {
+  const writing = fs.createWriteStream(path.join(__dirname, 'text.txt'), {
+    encoding: 'utf8',
+  });
+  const reading = Readable.from(stdin);
 
-console.log(
-  'Вводите текст, он будет сохранён в text.txt. Для завершения поставьте пустую строку:',
-);
+  console.log(
+    'Вводите текст, он будет сохранён в text.txt. Для завершения поставьте пустую строку:',
+  );
 
-reading.on('data', (chunk) => {
-  const text = `${chunk}`;
-  if (text === '\n') {
-    reading.destroy();
-    return;
-  }
-  writing.write(text);
-});
+  reading.on('data', (chunk) => {
+    const text = `${chunk}`;
+    if (text === '\n') {
+      reading.destroy();
+      return;
+    }
+    writing.write(text);
+  });
 
-reading.on('end', () => {
-  console.log('Текст сохранён в файл text.txt');
-  writing.end();
-});
+  reading.on('end', () => {
+    console.log('Текст сохранён в файл text.txt');
+    writing.end();
+  });
 
-process.on('SIGINT', () => {
-  console.log(' Текст сохранён в файл text.txt');
-  writing.end();
-  process.exit();
-});
+  process.on('SIGINT', () => {
+    console.log(' Текст сохранён в файл text.txt');
+    writing.end();
+    process.exit();
+  });
+} catch (err) {
+  console.error(err);
+}
